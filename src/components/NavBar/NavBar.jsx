f0690e1405368dc010b4f48849a1e36c779bb6d8
Micaela Analia Jofre <mkjofre@gmail.com>
2022-02-17 23:03:42 -0300

    navbar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './NavBar.css';
+import logo from '../../assets/image/logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartPlus, faEnvelope, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+
+const NavBar = () => {
+    return (
+        <div className='containerNav'>
+            <nav className='nav'>
+                <a href=""><img src={logo} alt="logo empresarial" /></a>
+                <ul>
+                    <li><a href=''>Deco</a></li>
+                    <li><a href=''>Bazar</a></li>
+                    <li><a href=''>Cuadros</a></li>
+                    <li><a href=''>Textil</a></li>
+                    <li><a href=''>Aromas</a></li>
+                </ul>
+                <div className='icons'>
+                    <a href=""><FontAwesomeIcon icon={faMagnifyingGlass} /></a>
+                    <a href=""><FontAwesomeIcon icon={faEnvelope} /></a>
+                    <a href=""><FontAwesomeIcon icon={faCartPlus} /></a>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export { NavBar }
