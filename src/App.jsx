b51ae7bfc2a31e6e9a6e6368eebccee14eec809e
Micaela Analia Jofre <mkjofre@gmail.com>
2022-04-17 18:44:35 -0300

    github4

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import './App.css';
 function App() {
 
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>>
+    <BrowserRouter basename={process.env.PUBLIC_URL}> 
       <UseContextAllIn >
         < div className="App" >
           <header>
